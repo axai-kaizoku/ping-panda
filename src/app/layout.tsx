9417e4a7ab51d4bcbb81412a400eedb4f63344c6
Akshay Yelle <02b3akshay@gmail.com>
2024-11-02 23:44:12 +0530

    add discord section 1/2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,8 +14,8 @@ const eb_garamond = EB_Garamond({
 })
 
 export const metadata: Metadata = {
-  title: "jStack App",
-  description: "Created using jStack",
+  title: "Ping Panda",
+  description: "Monitor your SaaS, easily !!",
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 }
 
