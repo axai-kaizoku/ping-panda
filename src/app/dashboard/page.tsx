5104d95370e8eac73c3a589f15cb481f44624843
Akshay Yelle <02b3akshay@gmail.com>
2024-11-07 12:22:32 +0530

    add auth, db, api w/tanstack query

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,21 @@
+import { db } from "@/db"
+import { currentUser } from "@clerk/nextjs/server"
+import { redirect } from "next/navigation"
+
+export default async function Page() {
+  const auth = await currentUser()
+
+  if (!auth) {
+    redirect("/sign-in")
+  }
+
+  const user = await db.user.findUnique({
+    where: { externalId: auth.id },
+  })
+
+  if (!user) {
+    redirect("/sign-in")
+  }
+
+  return <div>Dashboard</div>
+}
