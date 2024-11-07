2e6811cbfd3c1a08f2837e6c1f2bcf7bda8243fb
Akshay Yelle <02b3akshay@gmail.com>
2024-11-07 17:58:01 +0530

    add event categories, building form for creating event category

---
+++ src/app/dashboard/_components/create-event-category-modal.tsx
@@ -0,0 +1,127 @@
+"use client"
+import { useQueryClient } from "@tanstack/react-query"
+import { PropsWithChildren, useState } from "react"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import { CATEGORY_NAME_VALIDATOR } from "@/lib/validators/category-validators"
+import { Modal } from "@/components/ui/modal"
+import { Label } from "@/components/ui/label"
+import { Input } from "@/components/ui/input"
+import { cn } from "@/utils"
+
+const EVENT_CATEGORY_VALIDATOR = z.object({
+  name: CATEGORY_NAME_VALIDATOR,
+  color: z
+    .string()
+    .min(1, "Color is required")
+    .regex(/^#[0-9A-F]{6}$/i, "Invalid color format."),
+  emoji: z.string().emoji("Invalid Emoji").optional(),
+})
+
+type EventCategoryForm = z.infer<typeof EVENT_CATEGORY_VALIDATOR>
+
+const COLOR_OPTIONS = [
+  "#FF6B6B", // bg-[#FF6B6B] ring-[#FF6B6B] Bright Red
+  "#4ECDC4", // bg-[#4ECDC4] ring-[#4ECDC4] Teal
+  "#45B7D1", // bg-[#45B7D1] ring-[#45B7D1] Sky Blue
+  "#FFA07A", // bg-[#FFA07A] ring-[#FFA07A] Light Salmon
+  "#98D8C8", // bg-[#98D8C8] ring-[#98D8C8] Seafoam Green
+  "#FDCB6E", // bg-[#FDCB6E] ring-[#FDCB6E] Mustard Yellow
+  "#6C5CE7", // bg-[#6C5CE7] ring-[#6C5CE7] Soft Purple
+  "#FF85A2", // bg-[#FF85A2] ring-[#FF85A2] Pink
+  "#2ECC71", // bg-[#2ECC71] ring-[#2ECC71] Emerald Green
+  "#E17055", // bg-[#E17055] ring-[#E17055] Terracotta
+]
+
+const EMOJI_OPTIONS = [
+  { emoji: "💰", label: "Money (Sale)" },
+  { emoji: "👤", label: "User (Sign-up)" },
+  { emoji: "🎉", label: "Celebration" },
+  { emoji: "📅", label: "Calendar" },
+  { emoji: "🚀", label: "Launch" },
+  { emoji: "📢", label: "Announcement" },
+  { emoji: "🎓", label: "Graduation" },
+  { emoji: "🏆", label: "Achievement" },
+  { emoji: "💡", label: "Idea" },
+  { emoji: "🔔", label: "Notification" },
+]
+
+export default function CreateEventCategoryModal({
+  children,
+}: PropsWithChildren) {
+  const [isOpen, setIsOpen] = useState(false)
+  const queryClient = useQueryClient()
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    setValue,
+    formState: { errors },
+  } = useForm<EventCategoryForm>({
+    resolver: zodResolver(EVENT_CATEGORY_VALIDATOR),
+  })
+
+  const color = watch("color")
+
+  const onSubmit = (data: EventCategoryForm) => {}
+  return (
+    <>
+      <div onClick={() => setIsOpen(true)}>{children}</div>
+      <Modal
+        className="max-w-xl p-8"
+        showModal={isOpen}
+        setShowModal={setIsOpen}
+      >
+        <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
+          <div>
+            <h2 className="text-lg/7 font-medium tracking-tight text-gray-950">
+              New Event Category
+            </h2>
+            <p className="text-sm/6 text-gray-600">
+              Create a new category to organize your events.
+            </p>
+          </div>
+
+          <div className="space-y-5">
+            <div>
+              <Label htmlFor="name">Name</Label>
+              <Input
+                autoFocus
+                id="name"
+                {...register("name")}
+                placeholder="e.g. user-signup"
+              />
+              {errors.name ? (
+                <p className="mt-1 text-sm text-red-500">
+                  {errors.name.message}
+                </p>
+              ) : null}
+            </div>
+
+            <div>
+              <Label>Color</Label>
+              <div className="flex flex-wrap gap-3">
+                {COLOR_OPTIONS.map((premadeColor) => (
+                  <button
+                    key={premadeColor}
+                    type="button"
+                    className={cn(
+                      `bg-[${premadeColor}]`,
+                      "size-10 rounded-full, ring-2 ring-offset-2 transition-all",
+                      color === premadeColor
+                        ? "ring-brand-700 scale-110"
+                        : "ring-transparent hover:scale-105"
+                    )}
+                    onClick={() => setValue("color", premadeColor)}
+                  ></button>
+                ))}
+              </div>
+            </div>
+          </div>
+        </form>
+      </Modal>
+    </>
+  )
+}
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,11 @@
 import { db } from "@/db"
 import { currentUser } from "@clerk/nextjs/server"
 import { redirect } from "next/navigation"
+import DashboardMain from "./_components/dashboard-main"
+import DashboardContent from "./_components/dashboard-content"
+import CreateEventCategoryModal from "./_components/create-event-category-modal"
+import { Button } from "@/components/ui/button"
+import { PlusIcon } from "lucide-react"
 
 export default async function Page() {
   const auth = await currentUser()
@@ -17,5 +22,19 @@ export default async function Page() {
     redirect("/sign-in")
   }
 
-  return <div>Dashboard</div>
+  return (
+    <DashboardMain
+      cta={
+        <CreateEventCategoryModal>
+          <Button>
+            <PlusIcon className="size-4 mr-2" />
+            Add Category
+          </Button>
+        </CreateEventCategoryModal>
+      }
+      title="Dashboard"
+    >
+      <DashboardContent />
+    </DashboardMain>
+  )
 }
