b5f17dbe9cda0729aaff6aecbfd0e1bfeea18a01
Akshay <119416860+axai-kaizoku@users.noreply.github.com>
2024-11-11 16:03:28 +0530

    remove payments (#5)

--- src/app/(auth)/sign-in/[[...rest]]/page.tsx
+++ src/app/(auth)/sign-in/[[...rest]]/page.tsx
@@ -3,7 +3,7 @@ import { SignIn } from "@clerk/nextjs"
 export default function SignInPage() {
   return (
     <div className="w-full flex-1 flex items-center justify-center">
-      <SignIn />
+      <SignIn forceRedirectUrl={"/dashboard"} />
     </div>
   )
 }
